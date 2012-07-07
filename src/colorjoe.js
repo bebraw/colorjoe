cf6c161309ef1903ea04ff22a910d25072c124c3
Juho Vepsalainen <bebraw@gmail.com>
2012-07-07 15:40:22 +0300

    Fix 1 - issue at HSL picker
    The lightness axis should be inverted

--- src/colorjoe.js
+++ src/colorjoe.js
@@ -46,7 +46,7 @@ picker.hsl = picker({
     var ret = color.hsla(col);
 
     this.xy(ret, {x: ret.h(), y: 1 - ret.s()}, xy, z);
-    this.z(ret, ret.l(), xy, z);
+    this.z(ret, 1 - ret.l(), xy, z);
 
     return ret;
   },
