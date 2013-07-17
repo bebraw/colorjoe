a13bf93171507b4502762f38dc531ff06beacd5f
Juho Vepsalainen <bebraw@gmail.com>
2013-07-17 15:43:25 +0300

    Update version at dist

--- dist/colorjoe.js
+++ dist/colorjoe.js
@@ -12,7 +12,7 @@
 
     }(this, function () {
 
-        /*! colorjoe - v0.8.1 - Juho Vepsalainen <bebraw@gmail.com> - MIT
+        /*! colorjoe - v0.9.0 - Juho Vepsalainen <bebraw@gmail.com> - MIT
 https://bebraw.github.com/colorjoe - 2013-07-17 */
 /*! dragjs - v0.4.0 - Juho Vepsalainen <bebraw@gmail.com> - MIT
 https://bebraw.github.com/dragjs - 2013-07-17 */
