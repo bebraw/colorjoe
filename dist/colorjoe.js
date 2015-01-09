036db90c528b2e88c64bba353888b64f9b7c6dd7
Juho Vepsalainen <bebraw@gmail.com>
2015-01-09 09:26:14 +0200

    Bump version

--- dist/colorjoe.js
+++ dist/colorjoe.js
@@ -9,8 +9,8 @@
         root.colorjoe = factory(root.one.color);
     }
 }(this, function(onecolor) {
-/*! colorjoe - v0.9.7 - Juho Vepsalainen <bebraw@gmail.com> - MIT
-https://bebraw.github.com/colorjoe - 2014-12-07 */
+/*! colorjoe - v0.9.8 - Juho Vepsalainen <bebraw@gmail.com> - MIT
+https://bebraw.github.com/colorjoe - 2015-01-09 */
 /*! dragjs - v0.4.0 - Juho Vepsalainen <bebraw@gmail.com> - MIT
 https://bebraw.github.com/dragjs - 2013-07-17 */
 var drag = (function() {
