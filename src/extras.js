9748c1bbf05e53b7f4bc2a76993e701ae41e25c9
Juho Vepsäläinen <bebraw@gmail.com>
2022-11-26 13:32:20 +0200

    chore: Apply Prettier to code

--- src/extras.js
+++ src/extras.js
@@ -1,161 +1,163 @@
-var drag = require('dragjs');
-var utils = require('./utils');
+var drag = require("dragjs");
+var utils = require("./utils");
 
 function currentColor(p) {
-    var e1 = utils.div('currentColorContainer', p);
-    var e = utils.div('currentColor', e1);
+  var e1 = utils.div("currentColorContainer", p);
+  var e = utils.div("currentColor", e1);
 
-    return {
-        change: function(col) {
-            utils.BG(e, col.cssa());
-        }
-    };
+  return {
+    change: function (col) {
+      utils.BG(e, col.cssa());
+    },
+  };
 }
 
 function fields(p, joe, o) {
-    var cs = o.space;
-    var fac = o.limit || 255;
-    var fix = o.fix >= 0? o.fix: 0;
-    var inputLen = ('' + fac).length + fix;
-    inputLen = fix? inputLen + 1: inputLen;
+  var cs = o.space;
+  var fac = o.limit || 255;
+  var fix = o.fix >= 0 ? o.fix : 0;
+  var inputLen = ("" + fac).length + fix;
+  inputLen = fix ? inputLen + 1 : inputLen;
 
-    var initials = cs.split('');
-    var useAlpha = cs[cs.length - 1] == 'A';
-    cs = useAlpha? cs.slice(0, -1): cs;
+  var initials = cs.split("");
+  var useAlpha = cs[cs.length - 1] == "A";
+  cs = useAlpha ? cs.slice(0, -1) : cs;
 
-    if(['RGB', 'HSL', 'HSV', 'CMYK'].indexOf(cs) < 0) {
-        return console.warn('Invalid field names', cs);
-    }
+  if (["RGB", "HSL", "HSV", "CMYK"].indexOf(cs) < 0) {
+    return console.warn("Invalid field names", cs);
+  }
 
-    var c = utils.div('colorFields', p);
-    var elems = initials.map(function(n) {
-        n = n.toLowerCase();
+  var c = utils.div("colorFields", p);
+  var elems = initials.map(function (n) {
+    n = n.toLowerCase();
 
-        var e = utils.labelInput('color ' + n, n, c, inputLen);
-        e.input.onblur = done;
-        e.input.onkeydown = validate;
-        e.input.onkeyup = update;
-
-        return {
-            name: n, 
-            e: e
-        };
-    });
-
-    function done() {
-        joe.done();
-    }
-
-    function validate(e) {
-        if (!(e.ctrlKey || e.altKey) && /^[a-zA-Z]$/.test(e.key)) {
-            e.preventDefault();
-        }
-    }
-
-    function update() {
-        var col = [cs];
-
-        elems.forEach(function(o) {col.push(o.e.input.value / fac);});
-
-        if(!useAlpha) {
-            col.push(joe.getAlpha());
-        }
-
-        joe.set(col);
-    }
+    var e = utils.labelInput("color " + n, n, c, inputLen);
+    e.input.onblur = done;
+    e.input.onkeydown = validate;
+    e.input.onkeyup = update;
 
     return {
-        change: function(col) {
-            elems.forEach(function(o) {
-                o.e.input.value = (col[o.name]() * fac).toFixed(fix);
-            });
-        }
+      name: n,
+      e: e,
     };
+  });
+
+  function done() {
+    joe.done();
+  }
+
+  function validate(e) {
+    if (!(e.ctrlKey || e.altKey) && /^[a-zA-Z]$/.test(e.key)) {
+      e.preventDefault();
+    }
+  }
+
+  function update() {
+    var col = [cs];
+
+    elems.forEach(function (o) {
+      col.push(o.e.input.value / fac);
+    });
+
+    if (!useAlpha) {
+      col.push(joe.getAlpha());
+    }
+
+    joe.set(col);
+  }
+
+  return {
+    change: function (col) {
+      elems.forEach(function (o) {
+        o.e.input.value = (col[o.name]() * fac).toFixed(fix);
+      });
+    },
+  };
 }
 
 function alpha(p, joe) {
-    var e = drag.slider({
-        parent: p,
-        'class': 'oned alpha',
-        cbs: {
-            begin: change,
-            change: change,
-            end: done
-        }
-    });
+  var e = drag.slider({
+    parent: p,
+    class: "oned alpha",
+    cbs: {
+      begin: change,
+      change: change,
+      end: done,
+    },
+  });
 
-    function done() {
-        joe.done();
-    }
+  function done() {
+    joe.done();
+  }
 
-    function change(p) {
-        var val = utils.clamp(p.y, 0, 1);
+  function change(p) {
+    var val = utils.clamp(p.y, 0, 1);
 
-        utils.Y(p.pointer, val);
-        joe.setAlpha(1 - val);
-    }
+    utils.Y(p.pointer, val);
+    joe.setAlpha(1 - val);
+  }
 
-    return {
-        change: function(col) {
-            utils.Y(e.pointer, 1 - col.alpha());
-        }
-    };
+  return {
+    change: function (col) {
+      utils.Y(e.pointer, 1 - col.alpha());
+    },
+  };
 }
 
 function hex(p, joe, o) {
-    var e = utils.labelInput('hex', o.label || '', p, 7);
-    e.input.value = '#';
+  var e = utils.labelInput("hex", o.label || "", p, 7);
+  e.input.value = "#";
 
-    e.input.onkeyup = function(elem) {
-        var key = elem.keyCode || elem.which;
-        var val = elem.target.value;
-        val = val[0] == '#'? val: '#' + val;
-        val = pad(val, 7, '0');
+  e.input.onkeyup = function (elem) {
+    var key = elem.keyCode || elem.which;
+    var val = elem.target.value;
+    val = val[0] == "#" ? val : "#" + val;
+    val = pad(val, 7, "0");
 
-        if(key == 13) {
-            joe.set(val);
-        }
-    };
+    if (key == 13) {
+      joe.set(val);
+    }
+  };
 
-    e.input.onblur = function(elem) {
-        joe.set(elem.target.value);
-        joe.done();
-    };
+  e.input.onblur = function (elem) {
+    joe.set(elem.target.value);
+    joe.done();
+  };
 
-    return {
-        change: function(col) {
-            e.input.value = e.input.value[0] == '#'? '#': '';
-            e.input.value += col.hex().slice(1);
-        }
-    };
+  return {
+    change: function (col) {
+      e.input.value = e.input.value[0] == "#" ? "#" : "";
+      e.input.value += col.hex().slice(1);
+    },
+  };
 }
 
 function close(p, joe, o) {
-    var elem = utils.e('a', o['class'] || 'close', p);
-    elem.href = '#';
-    elem.innerHTML = o.label || 'Close';
+  var elem = utils.e("a", o["class"] || "close", p);
+  elem.href = "#";
+  elem.innerHTML = o.label || "Close";
 
-    elem.onclick = function(e) {
-        e.preventDefault();
+  elem.onclick = function (e) {
+    e.preventDefault();
 
-        joe.hide();
-    };
+    joe.hide();
+  };
 }
 
 function pad(a, n, c) {
-    var ret = a;
+  var ret = a;
 
-    for(var i = a.length; i < n; i++) {
-        ret += c;
-    }
+  for (var i = a.length; i < n; i++) {
+    ret += c;
+  }
 
-    return ret;
+  return ret;
 }
 
 module.exports = {
-    currentColor: currentColor,
-    fields: fields,
-    hex: hex,
-    alpha: alpha,
-    close: close
+  currentColor: currentColor,
+  fields: fields,
+  hex: hex,
+  alpha: alpha,
+  close: close,
 };
--- src/utils.js
+++ src/utils.js
@@ -1,86 +1,86 @@
-var div = partial(e, 'div');
+var div = partial(e, "div");
 
 function e(type, klass, p) {
-    var elem = document.createElement(type);
-    elem.className = klass;
-    p.appendChild(elem);
+  var elem = document.createElement(type);
+  elem.className = klass;
+  p.appendChild(elem);
 
-    return elem;
+  return elem;
 }
 
 // http://stackoverflow.com/questions/4394747/javascript-curry-function
 function partial(fn) {
-    var slice = Array.prototype.slice;
-    var args = slice.apply(arguments, [1]);
+  var slice = Array.prototype.slice;
+  var args = slice.apply(arguments, [1]);
 
-    return function() {
-        return fn.apply(null, args.concat(slice.apply(arguments)));
-    };
+  return function () {
+    return fn.apply(null, args.concat(slice.apply(arguments)));
+  };
 }
 
 function labelInput(klass, n, p, maxLen) {
-    var id = "colorPickerInput" + Math.floor(Math.random() * 1001);
-    var d = div(klass, p);
-    var l = label(n, d, id);
-    var i = input('text', d, maxLen, id);
+  var id = "colorPickerInput" + Math.floor(Math.random() * 1001);
+  var d = div(klass, p);
+  var l = label(n, d, id);
+  var i = input("text", d, maxLen, id);
 
-    return {
-        label: l, 
-        input: i
-    };
+  return {
+    label: l,
+    input: i,
+  };
 }
 
 function label(c, p, id) {
-    var elem = e('label', '', p);
-    elem.innerHTML = c;
+  var elem = e("label", "", p);
+  elem.innerHTML = c;
 
-    if (id) {
-        elem.setAttribute('for', id);
-    }
+  if (id) {
+    elem.setAttribute("for", id);
+  }
 
-    return elem;
+  return elem;
 }
 
 function input(t, p, maxLen, id) {
-    var elem = e('input', '', p);
-    elem.type = t;
+  var elem = e("input", "", p);
+  elem.type = t;
 
-    if(maxLen) { 
-        elem.maxLength = maxLen;
-    }
+  if (maxLen) {
+    elem.maxLength = maxLen;
+  }
 
-    if (id) {
-        elem.setAttribute('id', id);
-    }
+  if (id) {
+    elem.setAttribute("id", id);
+  }
 
-    if(maxLen) {
-        elem.maxLength = maxLen;
-    }
+  if (maxLen) {
+    elem.maxLength = maxLen;
+  }
 
-    return elem;
+  return elem;
 }
 
 function X(p, a) {
-    p.style.left = clamp(a * 100, 0, 100) + '%';
+  p.style.left = clamp(a * 100, 0, 100) + "%";
 }
 function Y(p, a) {
-    p.style.top = clamp(a * 100, 0, 100) + '%';
+  p.style.top = clamp(a * 100, 0, 100) + "%";
 }
 function BG(e, c) {
-    e.style.background = c;
+  e.style.background = c;
 }
 
 function clamp(a, minValue, maxValue) {
-    return Math.min(Math.max(a, minValue), maxValue);
+  return Math.min(Math.max(a, minValue), maxValue);
 }
 
 module.exports = {
-    clamp: clamp,
-    e: e,
-    div: div,
-    partial: partial,
-    labelInput: labelInput,
-    X: X,
-    Y: Y,
-    BG: BG
+  clamp: clamp,
+  e: e,
+  div: div,
+  partial: partial,
+  labelInput: labelInput,
+  X: X,
+  Y: Y,
+  BG: BG,
 };
